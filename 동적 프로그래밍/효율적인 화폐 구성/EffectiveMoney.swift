42c2771970c827d3c30d7aaa81a6769c70696721
Cho Sangho <97chos@naver.com>
2021-04-22 00:05:33 +0900

    주석 추가

--- 동적 프로그래밍/효율적인 화폐 구성/EffectiveMoney.swift
+++ 동적 프로그래밍/효율적인 화폐 구성/EffectiveMoney.swift
@@ -1,5 +1,8 @@
-let coinCount = Int(readLine()!)!
+// moneyCount = 화폐의 수량
+let moneyCount = Int(readLine()!)!
+// account = 목표 금액
 var account = Int(readLine()!)!
+// money = 화폐의 종류
 var money = readLine()!.split(separator: " ").map{ Int(String($0))! }
 
 // 금액의 크기만큼 배열을 생성한다. 이후 배열의 인덱스를 '금액'으로 고려하여 메모이제이션을 진행한다.
@@ -9,9 +12,11 @@ dp[0] = 0
 
 // 화폐의 수량만큼 1차 for문을 돌린다.
 for i in 0..<coinCount {
-  // 화폐의 수량이
+  // 화폐 금액부터 목표 금액만큼 2차 for문을 돌린다.
   for j in money[i]...account {
+    // 만약 현재 금액(인덱스 j)이 현재 for문에서 돌고 있는 화폐로 만들 수 있는 금액이라면 
     if dp[j - money[i]] != 10001 {
+      // 현재 금액으로 만드는데 들어가는 이전 화폐의 수량과 현재 금액으로 만드는데 들어가는 for문에서 돌고 있는 화폐의 수량을 비교하여 최소값으로 갱신한다.
       dp[j] = min(dp[j], dp[j - money[i]] + 1)
     }
   }
